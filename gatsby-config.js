7e5d024e3163fb69b2e82e080d958156c653e6bc
Zach Krall <zach@zachkrall.com>
2017-11-19 21:54:54 -0500

    init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: `Zach Krall`,
+  },
+  plugins: [`gatsby-plugin-react-helmet`],
+}
---
+++ src/pages/info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>About</h1>
+    <p>
+    Zach Krall (American, b. 1993 Japan) is a visual artist and creative technologist using images, video, and code to explore a contemporary understanding of the human condition.
+    </p>
+  </div>
+)
+
+export default IndexPage
